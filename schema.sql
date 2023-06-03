8acf806e884e4ddc6d8b024b9c8cbc329842e534
Rick Heist <rick.heist@gmail.com>
2023-06-03 00:30:10 -0400

    added seeds to employee and roles tables

--- schema.sql
+++ schema.sql
@@ -4,27 +4,27 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE departments (
-  id INT NOT NULL AUTO_INCREMENT,
+  id INT AUTO_INCREMENT NOT NULL,
   name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (id) NOT NULL
+  PRIMARY KEY(id)
 );
 
 CREATE TABLE roles (
-  id INT NOT NULL AUTO_INCREMENT,
+  id INT AUTO_INCREMENT NOT NULL,
   job_title VARCHAR(30) NOT NULL,
   departments_id INT NOT NULL,
   salary DECIMAL(7,0) NOT NULL,
-  PRIMARY KEY (id) NOT NULL,
+  PRIMARY KEY(id),
   FOREIGN KEY (departments_id) REFERENCES departments(id)  
 );
 
 CREATE TABLE employees (
-  id INT NOT NULL,
+  id INT AUTO_INCREMENT NOT NULL,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   roles_id INT NOT NULL,
   manager_id INT NULL,
-  PRIMARY KEY (id),
+  PRIMARY KEY(id),
   FOREIGN KEY (roles_id) REFERENCES roles(id),
   FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
--- seeds.sql
+++ seeds.sql
@@ -1,6 +1,29 @@
 INSERT INTO departments (name)
 VALUES 
+        ("Management"),
         ("Human Resources"),
         ("Sales"),
         ("Marketing"),
         ("Legal");
+
+INSERT INTO roles (job_title, departments_id, salary)
+VALUES
+        ("Manager", 1, 95000),
+        ("Training Coordinator", 2, 65000),
+        ("Employee Relations", 2, 60000),
+        ("Account Manager", 3, 130000),
+        ("Marketing Coordinator", 4, 80000),
+        ("Legal Counsel", 5, 250000),
+        ("Paralegal", 5, 65000);
+
+INSERT INTO employees (first_name, last_name, roles_id, manager_id)
+VALUES 
+        ("Maynard", "Keenan", 2, null),
+        ("Adam", "Jones", 3, 1),
+        ("Justin", "Chancellor", 3, null),
+        ("Danny", "Carey", 4, null),
+        ("Jerry", "Garcia", 4, 1),
+        ("Colonel", "Claypool", 5, 1),
+        ("Billy", "Strings", 6, 1),
+        ("Gene", "Ween", 6, null),
+        ("Dean", "Ween", 7, null);
