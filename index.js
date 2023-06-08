8da17d7cf536875e2b4febf75facd209e5a8488c
Rick Heist <rick.heist@gmail.com>
2023-06-07 23:08:12 -0400

    database is working. manager assigning is partially functional

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "Smawmk153",
-  database: 'employee_db'  
+  database: 'employee_db'
 });
 
-function mainMenu (){
-inquirer
+function mainMenu() {
+  inquirer
     .prompt([
       {
         type: 'list',
@@ -48,109 +48,178 @@ inquirer
             name: 'Quit',
             value: 'quit'
           },
-        ]        
+        ]
       }
     ]).then((data) => {
       if (['all-employees', 'all-roles', 'all-departments'].includes(data.selection))
-      getEmployeeData(data);
+        getEmployeeData(data);
       else if (['add-employee', 'add-role', 'add-department'].includes(data.selection))
-      addEmployeeData(data);
-      else if (data === 'update-employee-role')
-      updateEmployeeData(data);
+        addEmployeeData(data);
+      else if (data.selection === 'update-employee-role')
+        updateEmployeeData(data);
       else process.exit(0);
     });
-  }
-    
+}
+
 function getEmployeeData(data) {
   var query = '';
-  if (data.selection === 'all-employees'){
-    query = `SELECT * FROM employees`
-  } else if (data.selection === 'all-roles'){
-    query = `SELECT * FROM roles`
-  } else if (data.selection === 'all-departments'){
+  if (data.selection === 'all-employees') {
+    query = `SELECT e.id, e.first_name, e.last_name, r.job_title AS role, m.first_name AS manager_first_name, m.last_name AS manager_last_name FROM employees e JOIN roles r ON e.roles_id = r.id LEFT JOIN employees m ON e.manager_id = m.id`
+  } else if (data.selection === 'all-roles') {
+    query = `SELECT r.id, job_title, d.name AS department_name, salary FROM roles r JOIN departments d ON d.id = r.departments_id`
+  } else if (data.selection === 'all-departments') {
     query = `SELECT * FROM departments`;
   }
- db.query(query, (err, employeeData) => {
-    
+  db.query(query, (err, employeeData) => {
     console.table(employeeData);
     mainMenu();
-});
+  });
 };
 
 function addEmployeeData(data) {
-  console.log(data);
   if (data.selection === 'add-employee')
-  addNewEmployee();
+    addNewEmployee();
   else if (data.selection === 'add-role')
-  addNewRole();
+    addNewRole();
   else if (data.selection === 'add-department')
-  addNewDepartment();
+    addNewDepartment();
 };
 
-function addNewEmployee() {
+async function addNewEmployee() {
   var titles = [];
-  db.query(`SELECT job_title FROM roles`, (err, data) => {
-    console.log(data);
-    data.forEach(element => {
-      titles.push(element.job_title);    
-  });  
-  console.log(titles);
-  inquirer
+  var roleData = await db.promise().query(`SELECT id, job_title FROM roles`);
+  roleData[0].forEach(element => {
+    titles.push(element.job_title); 
+  })
+  var managers = ['NULL'];
+  var managerData = await db.promise().query(`SELECT first_name, last_name, id FROM employees WHERE employees.manager_id IS NULL`);
+  managerData[0].forEach(element => {
+    console.log(element.first_name);
+    var managerName = `${element.first_name} ${element.last_name}`;
+    console.log(managerName);
+    managers.push({name: managerName, value: element.id});
+    // managers.push(element.first_name)
+  }); 
+  var data = await inquirer
     .prompt([
       {
         type: 'input',
         name: 'firstName',
-        message: 'What is the employees first name?'    
+        message: 'What is the employee\'s first name?'
       },
       {
         type: 'input',
         name: 'lastName',
-        message: 'What is the employees last name?'   
+        message: 'What is the employee\'s last name?'
       },
       {
         type: 'list',
         name: 'role',
-        message: 'What is the employees role?',       
+        message: 'What is the employee\'s role?',
         choices: titles
       },
-    ]).then((data) => {
-      //add switch case for roles id to switch job title to role id
-      // switch (data.role)
-      // case 'Manager':
-        // role.id = 1;
-        //break;
-      // case 'Training Coordinator':
-        // role.id = 2;
-        //break
-      // case 'Employee Relations:
-        // role.id = 3;
-        //break;
-      //case 'Account Manager':
-        // role.id = 4;
-        //break
-      //case 'Marketing Coordinator':
-        //role.id = 5
-        //break
-      db.query(`INSERT INTO employees (first_name, last_name, roles_id) VALUES ("${data.firstName}", "${data.lastName}", "${data.role}")`, (err, data) => {
-        if (err) console.log(err);
-        console.table(data)
-        mainMenu();
-      });                              
-    });    
-  });
-  //mainMenu();
+      {
+        type: 'list',
+        name: 'managers',
+        message: 'Who is the employee\'s manager',
+        choices: managers
+      },
+    ])
+  var roleId;
+  roleData[0].forEach(element => {
+    if (element.job_title === data.role)
+      roleId = element.id;
+  })
+  console.log(data);
+  await db.promise().query(`INSERT INTO employees (first_name, last_name, roles_id, manager_id) VALUES ("${data.firstName}", "${data.lastName}", "${roleId}", "${data.managers}")`);
+  mainMenu();
 };
 
-// function addNewRole() {
-//   mainMenu();
-// };
+async function addNewRole() {
+  var departments = [];
+  var departmentData = await db.promise().query(`SELECT id, name FROM departments`);
+  departmentData[0].forEach(element => {
+    departments.push(element.name);
+  })
+  var data = await inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'jobTitle',
+        message: 'What is the new role title?'
+      },
+      {
+        type: 'input',
+        name: 'salary',
+        message: 'What is the new role\'s salary?'
+      },
+      {
+        type: 'list',
+        name: 'department',
+        message: 'What is the new role\'s department?',
+        choices: departments
+      },
+    ])
+  var departmentId;
+  departmentData[0].forEach(element => {
+    if (element.name === data.department)
+    departmentId = element.id;
+  })
+  await db.promise().query(`INSERT INTO roles (job_title, departments_id, salary) VALUES ("${data.jobTitle}", "${departmentId}", "${data.salary}")`);
+  mainMenu();
+};
 
-// function addNewDepartment() {
-//   mainMenu();
-// };
+async function addNewDepartment() {
+ var data = await inquirer
+    .prompt([
+      {
+      type: 'input',
+      name: 'newDepartment',
+      message: 'What is the new Department\'s name?'
+      },
+    ])
+  await db.promise().query(`INSERT INTO departments (name) VALUES ("${data.newDepartment}")`)
+  mainMenu();
+};
 
-// function updateEmployeeData(data) {
-//   mainMenu();
-// };
+async function updateEmployeeData(data) {
+  var employees = [];
+  var employeeData = await db.promise().query(`SELECT id, first_name, last_name, roles_id, manager_id FROM employees`);
+  employeeData[0].forEach(element => {
+    employees.push(`${element.first_name} ${element.last_name}`);
+  })
+  var roles = [];
+  var roleData = await db.promise().query(`SELECT id, job_title FROM roles`);
+  roleData[0].forEach(element => {
+    roles.push(element.job_title);
+  })
+  var data = await inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'employee',
+        message: 'Which employee would you like to edit?',
+        choices: employees
+      },
+      {
+        type: 'list',
+        name: 'role',
+        message: 'What is the employee\'s new role?',
+        choices: roles
+      },
+    ]);
+  var roleId;
+  roleData[0].forEach(element => {
+    if (element.job_title === data.role)
+      roleId = element.id;
+  })
+  var employeeId = '';
+  employeeData[0].forEach(element => {
+    if (`${element.first_name} ${element.last_name}` === data.employee)
+    employeeId = element.id;
+  })
+  await db.promise().query(`UPDATE employees SET roles_id = "${roleId}" WHERE id = "${employeeId}"`);
+  mainMenu();
+};
 
 mainMenu();
--- seeds.sql
+++ seeds.sql
@@ -1,6 +1,5 @@
 INSERT INTO departments (name)
 VALUES 
-        ("Management"),
         ("Human Resources"),
         ("Sales"),
         ("Marketing"),
@@ -8,22 +7,21 @@ VALUES
 
 INSERT INTO roles (job_title, departments_id, salary)
 VALUES
-        ("Manager", 1, 95000),
-        ("Training Coordinator", 2, 65000),
-        ("Employee Relations", 2, 60000),
-        ("Account Manager", 3, 130000),
-        ("Marketing Coordinator", 4, 80000),
-        ("Legal Counsel", 5, 250000),
-        ("Paralegal", 5, 65000);
+        ("Training Coordinator", 1, 65000),
+        ("Employee Relations", 1, 60000),
+        ("Account Manager", 2, 130000),
+        ("Marketing Coordinator", 3, 80000),
+        ("Legal Counsel", 4, 250000),
+        ("Paralegal", 4, 65000);
 
 INSERT INTO employees (first_name, last_name, roles_id, manager_id)
 VALUES 
-        ("Maynard", "Keenan", 2, null),
-        ("Adam", "Jones", 3, 1),
-        ("Justin", "Chancellor", 3, null),
-        ("Danny", "Carey", 4, null),
-        ("Jerry", "Garcia", 4, 1),
-        ("Colonel", "Claypool", 5, 1),
-        ("Billy", "Strings", 6, 1),
-        ("Gene", "Ween", 6, null),
-        ("Dean", "Ween", 7, null);
+        ("Maynard", "Keenan", 1, null),
+        ("Adam", "Jones", 2, 1),
+        ("Justin", "Chancellor", 2, 1),
+        ("Danny", "Carey", 3, 1),
+        ("Jerry", "Garcia", 3, 1),
+        ("Colonel", "Claypool", 4, 5),
+        ("Billy", "Strings", 5, 5),
+        ("Gene", "Ween", 5, 1),
+        ("Dean", "Ween", 6, 8);
